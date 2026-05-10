1459e56bc3b86d4e765db8848d51ffc82968a756
JozilovaZ <shnericaalp@gmail.com>
2026-05-11 00:34:16 +0500

    Fix Telegram bot duplicate: use gunicorn --preload to run bot once

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ echo ">>> Gunicorn ishga tushmoqda..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
+    --preload \
     --timeout 120 \
     --access-logfile - \
     --error-logfile -
