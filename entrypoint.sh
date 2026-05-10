28a5948acd5ece3cebcb251773f7c6abaa043a20
JozilovaZ <shnericaalp@gmail.com>
2026-05-11 00:36:47 +0500

    Use 1 gunicorn worker to prevent duplicate Telegram bot instances

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,7 @@ python manage.py yuklash
 echo ">>> Gunicorn ishga tushmoqda..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 2 \
-    --preload \
+    --workers 1 \
     --timeout 120 \
     --access-logfile - \
     --error-logfile -
